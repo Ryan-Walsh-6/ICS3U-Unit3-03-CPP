5c87b8c262cdfd8252f1cfee6b0d05dbb8736a03
Ubuntu <ubuntu@ip-172-31-84-227.ec2.internal>
2020-12-01 18:25:30 +0000

    improved_guessing_game.cpp

---
+++ improved_guessing_game.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2020 Ryan Walsh All rights reserved
+//
+// Created by Ryan Walsh
+// Created on November 30 2020
+// this program checks to see if the number guessed is the magic number
+
+#include <iostream>
+#include <random>
+
+int main() {
+    // this program checks to see if the number guessed is the magic number
+    int random_number;
+    int guessed_number;
+
+    // input
+    std::cout << "Enter a number(between 0-9)):";
+    std::cin >> guessed_number;
+    std::cout << "" << std::endl;
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  //  mersenne_twister
+    std::uniform_int_distribution<int> idist(0, 9);  //  [0,9]
+    random_number = idist(rgen);
+
+
+    // process and output
+    if (guessed_number == random_number) {
+        std::cout << "Congratulations! You guessed the right number!";
+    } else {
+        std::cout << "Not the correct number. Try again";
+        std::cout << "" << std::endl;
+        std::cout << "The correct number was:" << random_number << std::endl;
+    }
+}
